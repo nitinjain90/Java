10988291cacaf6db0127341eb0f17d0d7fef33c7
Nitin Jain <nitin@crispygam.es>
2016-03-15 13:17:14 +0530

    latest

---
+++ src/com/nitin/TNode.java
@@ -0,0 +1,44 @@
+package com.nitin;
+
+/**
+ * Created by harsh on 3/15/16.
+ */
+public class TNode {
+
+    private String data;
+    private TNode left;
+    private TNode right;
+
+    public TNode(String data) {
+
+    }
+
+    public void setLeft(TNode t) {
+
+    }
+
+    public void setRight(TNode t) {
+
+    }
+
+    public TNode getLeft() {
+        return null;
+    }
+
+    public TNode getRight() {
+        return null;
+    }
+
+    public boolean isLeaf() {
+        return false;
+    }
+
+    // Do using recursion.
+    public int height() {
+        return 0;
+    }
+
+    public int count() {
+        return 0;
+    }
+}
