349ef42d366554071c6e10d3f8edbef7b9ae5f19
Nitin Jain <nitin@crispygam.es>
2016-03-29 16:46:24 +0530

    latest

--- src/com/nitin/algorithms/SequenceAlgorithms.java
+++ src/com/nitin/algorithms/SequenceAlgorithms.java
@@ -10,22 +10,48 @@ public class SequenceAlgorithms {
     int data[];
 
     public SequenceAlgorithms(ArrayList<Integer> d) {
-        
-    }
 
+    }
     public SequenceAlgorithms(int[] d) {
-
+        this.data = d;
     }
-
     //
-    //    10 2 3 5 1 4 6 7
+    //    10 , 2 , 3 , 5 , 1 , 4 , 6 , 7
     public int longestIncreasingContinuous() {
+        int count[] = new int[data.length];
+        for(int k = 0; k < count.length; k++){
+            count[k] = 1;
+        }
+        
+
         return 0;
     }
 
     //   10 2 9 3 9 4 9 5 9 6
     public int longestIncreasing() {
-        return 0;
+        int count[] = new int[data.length];
+        for(int k = 0; k < count.length; k++){
+            count[k] = 1;
+        }
+        for(int i = 0; i < data.length; i++){
+            for(int j = i+1; j < data.length; j++){
+                if(data[j] > data[i]){
+                    count[i] = count[i] + 1;
+                }
+            }
+        }
+        int maxValue = count[0];
+        for(int i = 0; i < count.length; i++){
+            if(count[i] > maxValue){
+                maxValue = count[i];
+            }
+        }
+
+        return maxValue;
     }
 
+    public static void main(String args[]){
+        SequenceAlgorithms sq = new SequenceAlgorithms(new int[]{10 , 2 , 3 , 5 , 1 , 4 , 6 , 7 , 8 , 9 , 23 , 56});
+        System.out.println(sq.longestIncreasingContinuous());
+    }
 }
