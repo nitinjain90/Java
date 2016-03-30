0caf84581fb1be5c42abb303a77f6c0a3a575314
Nitin Jain <nitin@crispygam.es>
2016-03-30 14:27:34 +0530

    latest

--- src/com/nitin/tree/RedBlackTree.java
+++ src/com/nitin/tree/RedBlackTree.java
@@ -12,9 +12,9 @@ public class RedBlackTree {
 
     }
 
+
     public void addNumber(int N) {
         BSTNode n = new BSTNode(N);
-        insert_case1(n);
     }
 
     private static BSTNode getGrandParent(BSTNode n) {
@@ -60,62 +60,17 @@ public class RedBlackTree {
     }
 
     // when node is at the root of the tree
-    private static void insert_case1(BSTNode n) {
-        if (n.getParent() == null) {
-            n.setBlack();
-        } else {
-           insert_case2(n);
-        }
 
-    }
-    // if node parent is black. Everything is good
-    private static void insert_case2(BSTNode n) {
-       if(n.getParent().isBlack()){
-           return;
-       }else{
-         insert_case3(n);
-       }
-    }
-    //
-    private static void insert_case3(BSTNode n){
-         BSTNode u = getUncle(n);
-         BSTNode g;
-        if(u!=null && u.isRed()){
-            n.getParent().setBlack();
-            u.setBlack();
-            g = getGrandParent(n);
-            g.setRed();
-            insert_case1(g);
-        }else {
-           insert_case4(n);
-        }
-    }
-    private static void insert_case4(BSTNode n){
-        BSTNode g = getGrandParent(n);
-
-        if(n == n.getParent().getRight() && n.getParent() == g.getLeft()){
-            rotateLeft(n.getParent());
-            n = n.getLeft();
-        }else if(n == n.getParent().getLeft() && n.getParent() == g.getRight()){
-            rotateRight(n.getParent());
-            n = n.getRight();
-        }
-        insert_case5(n);
-
-    }
-    private static void insert_case5(BSTNode n){
-      BSTNode g = getGrandParent(n);
-        // Changing colors of P ang G to satisfy 4
-        n.getParent().setBlack();
-        g.setRed();
-        if(n == n.getParent().getLeft()){
-            rotateRight(g);
-        }else{
-            rotateLeft(g);
-        }
+    private static void myAssert(boolean condition) {
+        if (!condition) throw new IllegalStateException("Something is wrong");
     }
 
     public static void main(String args[]){
-        
+        RedBlackTree test = new RedBlackTree();
+        test.addNumber(43);
+        test.addNumber(56);
+        test.addNumber(27);
+
+      myAssert(test.root.isBlack());
     }
 }
