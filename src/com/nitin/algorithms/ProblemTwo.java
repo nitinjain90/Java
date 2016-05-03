94e2c18cd728c3066d0efeb19300815460efaf74
Nitin Jain <nitin@crispygam.es>
2016-05-03 15:47:08 +0530

    latest

--- src/com/nitin/algorithms/ProblemTwo.java
+++ src/com/nitin/algorithms/ProblemTwo.java
@@ -4,5 +4,5 @@ package com.nitin.algorithms;
  * Created by harash on 03/05/16.
  */
 public class ProblemTwo {
-        
+
 }
---
+++ src/com/nitin/algorithms/SearchingQuickly.java
@@ -0,0 +1,60 @@
+package com.nitin.algorithms;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Created by harash on 03/05/16.
+ */
+public class SearchingQuickly {
+
+    public static String printTitles(String s){
+        ArrayList<String> wordsToIgnore = new ArrayList<>();
+        ArrayList<String> otherStrings = new ArrayList<>();
+        ArrayList<String> keywords = new ArrayList<>();
+        int value = 0;
+        String[] temp = s.split("\\n");
+        int i = 0;
+        while(!temp[i].equals("::")){
+            wordsToIgnore.add(temp[i]);
+            i++;
+            value = i;
+
+        }
+        System.out.println(wordsToIgnore);
+        for(int j = value + 1; j < temp.length; j++){
+            otherStrings.add(temp[j]);
+        }
+        Set<String> keys = new TreeSet<>();
+        for(int  k = 0; k < otherStrings.size(); k++){
+            String[] t = otherStrings.get(k).split("\\s");
+            for(int l = 0; l < t.length; l++){
+                if(!wordsToIgnore.contains(t[l])){
+                    keys.add(t[l]);
+                }
+            }
+        }
+        System.out.println(otherStrings);
+        System.out.println(keys);
+
+        return "";
+    }
+
+    public static void main(String args[]){
+
+        printTitles("is\n" +
+               "the\n" +
+               "of\n" +
+               "and\n" +
+               "as\n" +
+               "a\n" +
+               "but\n" +
+               "::\n" +
+               "Descent of Man\n" +
+               "The Ascent of Man\n" +
+               "The Old Man and The Sea\n" +
+               "A Portrait of The Artist As a Young Man\n" +
+               "A Man is a Man but Bubblesort IS A DOG");
+    }
+}
