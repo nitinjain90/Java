e2b2abb4d19d32413e6c0c569078ce63db4f6701
Nitin Jain <nitin@crispygam.es>
2016-06-04 17:28:13 +0530

    latest

---
+++ src/com/nitin/acm/Problem131.java
@@ -0,0 +1,9 @@
+package com.nitin.acm;
+
+/**
+ * Created by harash on 04/06/16.
+ */
+//Psychic poker player
+public class Problem131 {
+
+}
---
+++ src/com/nitin/acm/Problem151.java
@@ -0,0 +1,13 @@
+package com.nitin.acm;
+
+import java.util.ArrayList;
+
+/**
+ * Created by harash on 04/06/16.
+ */
+
+// Power crisis
+public class Problem151 {
+    ArrayList<Integer> regions;
+    
+}
